4cb4edd38e9fe30b424c436b2cf6e204b0706abb
min33sky <min33sky@naver.com>
2023-03-06 15:02:37 +0900

    refactor: main ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
 import Generator from './components/Generator';
 import Reader from './components/Reader';
+import {
+  QrCodeIcon,
+  PlusIcon,
+  MagnifyingGlassIcon,
+} from '@heroicons/react/24/outline';
 
 const titleMap = {
-  Generator: 'QR Code Generator',
-  Reader: 'QR Code Reader',
+  Generator: 'QR 코드 생성기',
+  Reader: 'QR 코드 리더기',
 };
 
 type Tab = keyof typeof titleMap;
@@ -13,29 +18,54 @@ export default function App() {
   const [mode, setMode] = useState<Tab>('Reader');
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center  bg-gradient-to-tr from-slate-800 to-slate-500">
-      <div className="w-11/12 max-w-xl space-y-4 rounded-lg bg-slate-100 px-6 py-10">
-        <header className="flex flex-col space-y-2 ">
-          <h1 className="text-center text-3xl font-bold text-slate-700 ">
+    <main className="flex min-h-screen flex-col items-center justify-center  bg-gradient-to-tr from-cyan-900 to-cyan-600">
+      <div className="w-11/12 max-w-xl space-y-6 rounded-lg bg-cyan-50 px-4 py-6">
+        <header className="flex flex-col space-y-4 ">
+          <QrCodeIcon className="mx-auto h-12 w-12 text-cyan-600" />
+          <h1 className="text-center text-2xl font-bold text-cyan-700 ">
             {titleMap[mode]}
           </h1>
-          <nav className="flex justify-end gap-4">
+
+          <nav className="flex space-x-1">
             <button
-              className="rounded-md bg-slate-700 px-5 py-2 tracking-wider text-slate-100 transition hover:bg-slate-900"
+              aria-label='Button to switch to "Generator" mode'
+              title="생성 모드로 전환"
+              className={`flex flex-1 items-center justify-center space-x-1 rounded-md border px-5 py-2 tracking-widest transition duration-200 ${
+                mode === 'Generator'
+                  ? 'bg-cyan-600 text-cyan-100'
+                  : 'border-cyan-600 bg-cyan-50 text-cyan-600'
+              }`}
               onClick={() => setMode('Generator')}
             >
-              생성
+              <PlusIcon className="h-6 w-6" />
+              <p
+                className={`${
+                  mode === 'Generator' ? 'inline-block' : 'hidden'
+                }`}
+              >
+                Create
+              </p>
             </button>
+
             <button
-              className="rounded-md bg-slate-700 px-5 py-2 tracking-wider text-slate-100 transition hover:bg-slate-900"
+              aria-label='Button to switch to "Reader" mode'
+              title="읽기 모드로 전환"
+              className={`flex flex-1 items-center justify-center space-x-1 rounded-md border px-5 py-2 tracking-widest transition duration-200 ${
+                mode === 'Reader'
+                  ? 'bg-cyan-600 text-cyan-100'
+                  : 'border-cyan-600 bg-cyan-50 text-cyan-600'
+              }`}
               onClick={() => setMode('Reader')}
             >
-              읽기
+              <MagnifyingGlassIcon className="h-6 w-6" />
+              <p className={`${mode === 'Reader' ? 'inline-block' : 'hidden'}`}>
+                Read
+              </p>
             </button>
           </nav>
         </header>
 
-        <div className="mx-auto h-0.5 w-full  bg-slate-400"></div>
+        <div className="mx-auto h-0.5 w-full  bg-cyan-600"></div>
 
         <section>
           {mode === 'Generator' && <Generator />}
